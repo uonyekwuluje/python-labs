1361e548d285a258e192dec4e21e436d8eb233df
uonyekwuluje <uionyekwuluje@gmail.com>
2026-08-01 18:01:08 -0400

    add test code

--- sample.test.py
+++ sample.test.py
@@ -1,3 +1,21 @@
+[
+  '192.168.45.201',
+  '8.213.117.44',
+  '104.76.23.181',
+  '172.33.90.7',
+  '54.201.199.150'
+]
+
+
+[
+    '6fe4:2b2c:8a97:4a4b:ea78:de0d:5d7d:56f3',
+    '24e7:4d2a:b5e6:ea2f:81cb:98fd:ef6:db80',
+    'fc19:28e7:f03b:5e0f:ad6d:92e3:5d51:ba95',
+    '3d9b:1a85:cf9c:ae80:4cbf:df72:18b1:34d0',
+    'bf0c:53b3:de2d:f4c4:8a3e:bc68:7b56:1142'
+] 
+
+
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
 print(thislist[:4])
